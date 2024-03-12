91b9746e4eae8f58a77665bdf68fcd0a35426228
Magnewei <magnusnorgaardweihager@Magnuss-MacBook-Pro.local>
2024-03-12 16:17:39 +0100

    Template fix

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Javalin template
+
+### 1. Contains a connection pool wrapper utilizing a Hakari connection pool.
+
+### 2. Contains example of Database mappers barebones Thymeleaf html pages.
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,16 +6,16 @@ import org.example.Persistence.ConnectionPool;
 import org.example.Thymeleaf.ThymeleafConfig;
 
 public class Main {
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
+    private static final String USER = "USERNAME";
+    private static final String PASSWORD = "PASSWORD";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
     private static final String DB = "databasename";
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
     public static void main(String[] args) {
-        // Initializing Javalin and Jetty webserver
 
+        // Initializing Javalin and Jetty webserver
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add("/public");
             config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
